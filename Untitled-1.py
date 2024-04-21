9b67af04383fdf6a83abd494a843204f4e81bbcf
7777777&Aa <romanvlasiuk90@gmail.com>
2024-04-21 13:17:43 +0300

    delete

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-is_next = None
-num = int(input("Enter the number of points: "))
-
-if num > 82:
-#    is_next = True
-    print ("Вітаємо у наступному турі")
-else:
-#   is_next = False
-    print ("Надто мало балів")
