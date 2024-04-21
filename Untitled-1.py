f6ad1a42b7200126f94d61426b3ba0dcee6d3c97
7777777&Aa <romanvlasiuk90@gmail.com>
2024-04-21 13:14:45 +0300

    https://github.com/romanvlasiuk90/goit-algo-hw-03

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+is_next = None
+num = int(input("Enter the number of points: "))
+
+if num > 82:
+#    is_next = True
+    print ("Вітаємо у наступному турі")
+else:
+#   is_next = False
+    print ("Надто мало балів")
